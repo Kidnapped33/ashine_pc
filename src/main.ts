dd39b8538df39eb57165f91b123494c77a0e3e72
ashine33 <ashine33@foxmail.com>
2022-10-13 10:49:08 +0800

    feat：add vue-router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,20 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import Login from "./views/Login.vue";
+import Home from "./views/Home.vue";
+import { createRouter, createWebHashHistory } from "vue-router";
 
-createApp(App).mount('#app')
+const routes = [
+  { path: "/", component: Login },
+  { path: "/Home", component: Home },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
